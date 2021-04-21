655686579c44c9a1929d1117a363f9a7a0ae8499
anhbui159 <33772016+anhbui159@users.noreply.github.com>
2021-04-21 00:27:32 -0400

    Pie Chart
    simple pie chart for countries added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { GlobalDataSummary } from './../../models/global-data';
+import { GoogleChartInterface } from 'ng2-google-charts';
+import { Ng2GoogleChartsModule, GoogleChartsSettings } from 'ng2-google-charts';
 
 @Component({
   selector: 'app-home',
@@ -13,14 +15,32 @@ export class HomeComponent implements OnInit {
   totalDeaths = 0;
   totalRecovered = 0;
   globalData: GlobalDataSummary[] = [];
+  pieChart: GoogleChartInterface = {
+    chartType: 'PieChart',
+  };
   constructor(private dataService: DataServiceService) {}
 
+  initChart() {
+    let datatable: any = [];
+    datatable.push(['Country', 'Cases']);
+    console.log(this.globalData);
+    this.globalData.forEach((cs) => {
+      datatable.push([cs.country, cs.confirmed]);
+    });
+    console.log(datatable);
+    this.pieChart = {
+      chartType: 'PieChart',
+      dataTable: datatable,
+      //firstRowIsData: true,
+      // options: { title: 'Tasks' },
+    };
+  }
   ngOnInit(): void {
     this.dataService.getGlobalData().subscribe({
       next: (result) => {
         // console.log(result);
         this.globalData = result;
-        result.forEach((cs: GlobalDataSummary) => {
+        this.globalData.forEach((cs: GlobalDataSummary) => {
           if (!Number.isNaN(cs.confirmed)) {
             this.totalActive += cs.active;
             this.totalConfirmed += cs.confirmed;
@@ -28,6 +48,7 @@ export class HomeComponent implements OnInit {
             this.totalRecovered += cs.recovered;
           }
         });
+        this.initChart();
       },
     });
   }
